d0ae0d1c0a1fbf1fd0325f68c2d978dc8b48321e
bkparz3 <benkparzych@gmail.com>
2024-10-17 17:22:19 -0700

    ../ on lab links

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -19,3 +19,23 @@ function main() {
 
 // let's get this party started
 main();
+//Rock_Paper_Scissors:
+//Give user three options to choose from Rock, Paper, Or Scissors
+  //Print image of rock, image of paper, and image of scissors from left to right
+//Accept user’s input
+//Random generate computer response from Rock, Paper or Scissors
+//Reveal computer’s choice to user
+  //Move User’s choice to left
+  //Print computers choice next to User’s on the right 
+//Determine win, loss, or draw
+  //Win condition, player chooses Rock if computer chooses Scissors
+  //Win condition, player chooses Paper if computer chooses Rock
+  //Win condition, player chooses Scissors if computer chooses Paper
+  //Lose condition, player chooses Rock if computer chooses Paper
+  //Lose condition, player chooses Paper if computer chooses Scissors
+  //Lose condition, player chooses Scissors if computer chooses Rock
+  //Draw Condition, player and computer have chosen the same option
+//If draw, repeat from step one
+//If win or loss, display appropriate tag according to win or loss
+  //Print “You win!” if user wins
+  //Print “You lose!” if user loses
