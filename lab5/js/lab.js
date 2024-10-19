95e992d650504a40c68b40bca540d57a3a10dfb8
bkparz3 <benkparzych@gmail.com>
2024-10-19 11:26:55 -0700

    Javascript stuff

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -20,3 +20,19 @@ function main() {
 
 // let's get this party started
 main();
+// Define Variables
+make = "Subaru";
+model = "Forester";
+color = "red";
+year = 2021;
+
+// calculate
+age = 2024 - year;
+
+//output 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
