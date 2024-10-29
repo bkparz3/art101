004d1dd5bae4fb27624eb92b5bb95e31d0e79d75
bkparz3 <benkparzych@gmail.com>
2024-10-28 17:38:21 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -15,3 +15,65 @@ function main() {
 
 // let's get this party started
 main();
+
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+
+ 
+  while (0 !== currentIndex) {
+     
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+
+     
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+
+
+function toTitleCase(str) {
+  return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+);
+}
+
+
+function randomizeName(userName) {
+
+var nameArray = userName.toLowerCase().split("");
+console.log("nameArray =", nameArray);
+
+var shuffledArray = shuffleArray(nameArray);
+console.log("shuffledArray =", shuffledArray);
+var shuffledString = shuffledArray.join("");
+
+var newName = toTitleCase(shuffledString);
+
+
+return newName;
+}
+
+
+function main() {
+var userName = window.prompt("Hello, Whats you name? I want it.");
+document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+var randomName = randomizeName(userName);
+document.writeln("Just for you!");
+document.writeln("<div class='name'>" + randomName + "</div>")
+}
+
+
+
+
+main();
+
