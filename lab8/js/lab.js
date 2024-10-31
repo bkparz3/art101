33d4d1e3c715d689f4aed963b22352150a1cb071
bkparz3 <benkparzych@gmail.com>
2024-10-30 17:26:44 -0700

    result

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -40,4 +40,4 @@ console.log("Squareroot of array:", result);
  var mapResults = "Your map results here";
 
  // Use jQuery to select the element by its ID and set the HTML content
- $("#output").html(mapResults);
+ $("#output").html(result);
