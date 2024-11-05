a04e37e4856a630b397a1ab16a80c86cdda053be
bkparz3 <benkparzych@gmail.com>
2024-11-04 19:10:43 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -30,7 +30,7 @@ $("#problems").append("<button id='button-problems'>Make Special</button>");
 // add a click listener to the challenge button
 $("#button-problems").click(function(){
   // now add (or subtract) the "special" class to the section
-  $("problems").toggleClass("special");
+  $("#problems").toggleClass("special");
 });
 
 // add button to challenge section
