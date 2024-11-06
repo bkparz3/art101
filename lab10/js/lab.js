f91c066c07c4594804229d581b76752225ee5d6c
bkparz3 <benkparzych@gmail.com>
2024-11-06 15:57:38 -0800

    lab 10 components

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,4 +1,4 @@
-// index.js - Libraries and JQuery
+// index.js - JavaScript for the Web
 // Author: ben parzych <bparzych@ucsc.edu>
 // Date: October 2024
 
@@ -15,3 +15,20 @@ function main() {
 
 // let's get this party started
 main();
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+// click listener for button
+$("#make-convo").click(function(){
+});
+// get new fake dialogue
+const newText = generateRandomText();
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
