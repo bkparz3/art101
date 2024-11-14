d9cc66e4f37baa5acc39c0efa037a297f1b80164
bkparz3 <benkparzych@gmail.com>
2024-11-13 16:19:54 -0800

    lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -15,3 +15,27 @@ function main() {
 
 // let's get this party started
 main();
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+   else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorthing Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
