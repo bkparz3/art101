5e0940b6f08aa6ac57bff37afd0ecb78c4c0d55c
bkparz3 <benkparzych@gmail.com>
2024-11-14 18:08:52 -0800

    blah blah

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -15,3 +15,45 @@ function main() {
 
 // let's get this party started
 main();
+function fizzBuzzBoom(maxNums, factorObj) {
+  // iterate over all of out numbers
+  for (var num=0; num<maxNums; num++) {
+    // reset output string
+    var outputStr = "";
+    //iterate over the factor we got from the html
+    for (var factor in factorObj) {
+      // check to see if this num is a multiple of factor
+      if (num % factor == 0) {
+        // if yes, than add the text to output string
+        outputStr += factorObj[factor];
+      }
+    }
+    //now if we have words in outputStr, format it like this " - FizzBuzz!"
+    if (outputStr) {
+      outputStr = " - " + outputStr + "!";
+    }
+    outputToPage(num.toString() + outputStr)
+  }
+}
+
+function reportError(str) {
+  outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
+document.getElementById("submit").addEventListener("click", function() {
+  var max = document.getElementById("max").value;
+  console.log("max:",max)
+  if (! max) {
+    reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    // clear error if there is one
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
+})
