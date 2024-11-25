ec1a20219a1a1ee7beb96addeb8011e7ab3a0da4
bkparz3 <benkparzych@gmail.com>
2024-11-25 10:54:13 -0800

    lab15

---
+++ lab15/js/lab.js
@@ -0,0 +1,17 @@
+// index.js - AJAX
+// Author: ben parzych <bparzych@ucsc.edu>
+// Date: November 2024
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
