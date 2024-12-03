315d69ec35c2001d292793f171c3999113dc7882
bkparz3 <benkparzych@gmail.com>
2024-12-02 19:05:38 -0800

    lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -13,6 +13,33 @@ function main() {
   // the code that makes everything happen
 }
 
-// let's get this party started
-main();
+// Define the API endpoint
+let endpoint = "https://api.nasa.gov/planetary/apod";
 
+// Configure the AJAX request
+let ajaxConfig = {
+  url: endpoint,  // API URL
+  method: "GET", // HTTP method
+  contentType: "application/json", // Payload type
+  data: { 
+    api_key: "MVjp3RUBIwTDw51jL0jBqriS5aS6HD1yXe1cflGT", // API token
+    
+  },
+  success: function(data) { // Success handler
+    
+    let record = data[0];
+    
+    console.log(data); 
+    $("#output").append("<h2>" + data.title);
+    $("#output").append("<p>" + data.explanation);
+    // display data.url
+    $("#output").append("<img src='"+ data.hdurl +"'>");
+    // add fields to output w append
+  }, 
+  error: function(xhr, status, error) { // Error handler
+    console.error(error); 
+  } 
+};
+
+// Send the AJAX request
+$.ajax(ajaxConfig);
